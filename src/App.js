67ab43f3aa9c0d4d79eee9c027169d0bcf8d3bce
javier martinicorena <martinicorenaj@gmail.com>
2021-06-08 12:16:13 -0300

    e 2.11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+
 
 const Persons=({showFilter,persons,nameToShow})=> {
 if (showFilter!==('')) {
@@ -30,15 +32,19 @@ return (
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([])
+
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showFilter,setShowFilter]= useState('')
+
+  useEffect (()=>{
+    axios
+         .get('http://localhost:3001/persons')
+         .then(response=>{
+          setPersons(response.data)
+         })
+  },[])
 
   const addNameYNumber=(event)=>{
    event.preventDefault()
