fe6180e01526ddffe06b1e0bcf6702d702369d2e
javier martinicorena <martinicorenaj@gmail.com>
2021-06-10 15:30:12 -0300

    2.13

--- src/App.js
+++ src/App.js
@@ -1,50 +1,81 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const BasicInfo=({showFilter,countries, nameToShow})=> {
-const arg=countries.filter(countrie=>countrie.name==="Argentina")
-console.log('que tiene arg',arg)
 
+const Pais=({countrie})=>{
+ return (
+  <div>
+   <h3>{countrie.name}</h3>
+   <p>capital {countrie.capital}</p>
+   <p>population {countrie.population}</p>
+   <img src={countrie.flag} alt='bandera' width="70" height="50"/>
+   <h3>Language</h3>
+   <ul>
+      {countrie.languages.map(language=>
+    <li key={countrie.name}>
+        {language.name}
+    </li>
+    )}
+   </ul>
+</div>
+  )
+}
+
+const BasicInfo=({showFilter, countries, nameToShow, pais, handleClick})=> {
+ 
+
+  
 if (nameToShow.length>10) {
 
  return (
  <p>to many matches especify another filter</p>
  )}
 
-if (nameToShow.length<10) {
+if (nameToShow.length<10&&nameToShow.length>1) {
 return (
   <div>
-        <ul>
-       {nameToShow.map(countrie=>
-      
+    <ul>
+      {nameToShow.map(countrie=>
        <li key={countrie.name}>
-       {countrie.name} 
+        {countrie.name} 
+        <button onClick={()=>handleClick(countrie)}>
+        show
+        </button>
        </li>
       )}
-      </ul>
-  </div>
- )}
- if (nameToShow.length>10) {
+    </ul>
 
- return (
- <p>to many matches especify another filter</p>
+   </div>
  )}
+console.log('pais', pais)
 
-if (nameToShow.length===1) {
+if (nameToShow.length<=1) {
   return (
-  <div>hola javier</div>
-  )
-} 
-  
+  <div>
+    {nameToShow.map(countrie=>
+     <Pais key={countrie.name} countrie={countrie} />
+    )}
+  </div>
+ )} 
 
+ 
 }
 
 
 const App=() => {
 
+
   const [countries, setCountries]= useState([])
 
   const [showFilter,setShowFilter]= useState('')
+  
+  const [pais, setNewpais]=useState(null)
+
+  const handleClick=(countrie)=>{
+  setNewpais(countrie)
+  console.log('pais',countrie)
+
+ }
 
   useEffect (()=>{
     axios
@@ -54,16 +85,14 @@ const App=() => {
          })
   },[])
 
-const handleSearchChange=(event)=>{
+const handleSearchChange = (event)=>{
     setShowFilter(event.target.value)
+ }
 
-
-    
-
-  }
-const nameToShow = countries.filter(countrie=>countrie.name.toLowerCase().includes(showFilter.toLowerCase()))
-
-//usar includes
+ const nameToShow = countries.filter(countrie=>
+    countrie.name.toLowerCase().includes(showFilter.toLowerCase())
+    )
+ console.log('condicion',!(nameToShow.length===1))
 
   return (
   <div>
@@ -73,7 +102,13 @@ const nameToShow = countries.filter(countrie=>countrie.name.toLowerCase().includ
              onChange={handleSearchChange}
         />
       </div>
-  <BasicInfo showFilter={showFilter} countries={countries} nameToShow={nameToShow} />
+   <BasicInfo showFilter={showFilter} countries={countries} nameToShow={nameToShow} pais={pais} handleClick={handleClick}/>
+   <div>{(pais&&!(nameToShow.length===1)) &&
+  <div>
+     <Pais key={pais.name} countrie={pais} />
+  </div>
+ } </div>
+
      </div>
   )
 }
